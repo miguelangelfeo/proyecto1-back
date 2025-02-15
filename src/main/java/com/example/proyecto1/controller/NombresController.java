c776eaad4ba75f5a0a283f61752d42d7cf34c157
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2025-02-15 09:22:51 -0500

    Arregla rutas

--- src/main/java/com/example/proyecto1/controller/NombresController.java
+++ src/main/java/com/example/proyecto1/controller/NombresController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://104.248.106.174", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RestController
 @AllArgsConstructor
 
